5c6b97b86575f7b1f503cb34458d2ed24d90b655
babagapi001 <108281477+babagapi001@users.noreply.github.com>
2025-07-05 18:10:59 +0800

    Create ZEq2gCJYRt.js

---
+++ ZEq2gCJYRt.js
@@ -0,0 +1,12 @@
+window.ACCOUNTS = {
+  "Qd7PvM2XKaTe6Lwb": "yV3pQ8BxdXKA7tMULzWcFrJg",
+  "XrA4yBtNdVq92WLC": "zMN6WbLpCYf9A3XvQJetTksu",
+  "hbT2MLQdpVXYxC9R": "eF6WKgCzBp9NXTmVuJyQaRLD",
+  "wnJrPVXtqLBAmcz5": "XuKbWZEq2gCJYRtL7DvNMa93",
+  "dRQ7ayvPbFJMczXK": "AcdTYwPJ9VmxQKB7LrzNEGuX",
+  "7PvFZqRmwaBtXN3g": "rKYUWQaPM9zLnDJ3vcBXeg5T",
+  "TBcJpzyoNLaF68Xq": "YLtUmgKeA7ZrwDpXBvPq39CJ",
+  "kMQByn9FrTCpVWex": "zPYRAvqwDLK73muXCFEVgJnB",
+  "zxMbQNLcPA6TyvF7": "cBrMUWptKVG7XYfqEdJnL9AZ",
+  "VzyRPJFLmaTw6qXN": "nYTRuGpvXMWKjZqA43EDLBxf"
+};
